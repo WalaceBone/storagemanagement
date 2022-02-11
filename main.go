201fca037edec2158cb9dd0248fb574e76bb3331
Guillaume Roux <guillaume3.roux@epitech.eu>
2022-02-11 14:33:11 +0100

    Added file opening and reading

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Printf("Hello World")
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("Program takes one argument: path of the file")
+		return
+	}
+	path := args[1]
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	file.Close()
+	for _, line := range lines {
+		fmt.Println(line)
+	}
 }
